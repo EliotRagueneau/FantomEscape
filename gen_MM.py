c7aa3343a2202752f9aac022796a1ddde3493bfa
Maxime Lecomte <maxime.lecomte06@free.fr>
2018-12-02 18:03:58 +0100

    commentaire

--- gen_MM.py
+++ gen_MM.py
@@ -3,17 +3,19 @@ from main_graphics import *
 
 
 def _gen_random():
-    matrice_ori = Game._gen_basic()
+    """ Cette fonction permet de creer la map de facon aleatoire """
+    matrice_ori = Game._gen_basic() # Importation de la matrice d'origine
     liste_cases = []
-    for ligne in matrice_ori:
+    for ligne in matrice_ori: # Parcours de la matrice original
         for case in ligne:
             if case not in liste_cases:
-                liste_cases.append(case)
-                
-    mm_haut = MarkovModel(liste_cases)
+                liste_cases.append(case) # Ajout des elements de la matrice original dans liste_cases
+
+    mm_haut = MarkovModel(liste_cases) # Création chaîne de markov
     mm_droite = MarkovModel(liste_cases)
     mm_gauche = MarkovModel(liste_cases)
-    for y in range(len(matrice_ori) - 1, 0, -1):
+
+    for y in range(len(matrice_ori) - 1, 0, -1): #
         for x in range(len(matrice_ori[0])):
             mm_haut[matrice_ori[y][x]][matrice_ori[y - 1][x]] += 1
 
@@ -27,8 +29,8 @@ def _gen_random():
     for _ in range(6):
 
         for ligne in matrice:
-            weights = mm_gauche[ligne[0]].values()
-            ligne.insert(0, rd.choices(liste_cases, weights=weights)[0])
+            weights = mm_gauche[ligne[0]].values() # Ajout d'un poids a chaque element du dictionnaire
+            ligne.insert(0, rd.choices(liste_cases, weights=weights)[0]) # insert un element de list_case en position 0
             weights = mm_droite[ligne[-1]].values()
             ligne.append(rd.choices(liste_cases, weights=weights)[0])
 
@@ -38,7 +40,7 @@ def _gen_random():
             new_top_line.append(rd.choices(liste_cases, weights=weights)[0])
         matrice.insert(0, new_top_line)
 
-        for line in matrice:
+        for line in matrice: # Affichage des lignes de la matrice
             print(line)
         print()
 
